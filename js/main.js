de3e551cc330c1918182822c553d0f32b8949b44
Jordan King <105494965+nomarjay@users.noreply.github.com>
2023-06-12 18:33:26 -0400

    hamburger menu
    added hamburger menu and corrected card names

--- js/main.js
+++ js/main.js
@@ -1,49 +1,56 @@
-// Get the container element
-const container = document.querySelector('.container');
+$(document).ready(function() {
 
-// Get all the children elements of the container
-const children = container.children;
+  // indent section
+  const container = document.querySelector('.container');
 
-// Loop through the children elements
-for (let i = 0; i < children.length; i++) {
-  const child = children[i];
+  const children = container.children;
 
-  // Check if the child does not have the class name "hero"
-  if (!child.classList.contains('hero, hero2, hero3')) {
-    // Apply the CSS class to indent the content
-    child.classList.add('indent');
+  for (let i = 0; i < children.length; i++) {
+    const child = children[i];
+
+    if (!child.classList.contains('hero, hero2, hero3')) {
+      child.classList.add('indent');
+    }
   }
-}
 
 
-// lazy load images
-document.addEventListener('DOMContentLoaded', function() {
-  var lazyImages = document.querySelectorAll('.lazy-load');
+  // lazy load images
+  document.addEventListener('DOMContentLoaded', function() {
+    var lazyImages = document.querySelectorAll('.lazy-load');
 
-  if ('IntersectionObserver' in window) {
-    var observer = new IntersectionObserver(function(entries) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          var lazyImage = entry.target;
-          lazyImage.src = lazyImage.dataset.src;
-          lazyImage.removeAttribute('data-src');
-          lazyImage.classList.remove('lazy-load');
-          observer.unobserve(lazyImage);
-        }
+    if ('IntersectionObserver' in window) {
+      var observer = new IntersectionObserver(function(entries) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            var lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.removeAttribute('data-src');
+            lazyImage.classList.remove('lazy-load');
+            observer.unobserve(lazyImage);
+          }
+        });
       });
-    });
 
-    lazyImages.forEach(function(lazyImage) {
-      observer.observe(lazyImage);
-    });
-  } else {
-    // Fallback for browsers that do not support Intersection Observer
-    // You can load all images immediately if needed
-    lazyImages.forEach(function(lazyImage) {
-      lazyImage.src = lazyImage.dataset.src;
-      lazyImage.removeAttribute('data-src');
-      lazyImage.classList.remove('lazy-load');
-    });
-  }
-});
+      lazyImages.forEach(function(lazyImage) {
+        observer.observe(lazyImage);
+      });
+    } else {
+      // Fallback for browsers that do not support Intersection Observer
+      // You can load all images immediately if needed
+      lazyImages.forEach(function(lazyImage) {
+        lazyImage.src = lazyImage.dataset.src;
+        lazyImage.removeAttribute('data-src');
+        lazyImage.classList.remove('lazy-load');
+      });
+    }
+  });
 
+
+  // hamburger menu
+  $(document).ready(function() {
+    $('.hamburger').click(function() {
+      $('.menu').slideToggle();
+    });
+  });
+
+})
